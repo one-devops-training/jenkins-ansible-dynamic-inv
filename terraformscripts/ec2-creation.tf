208659f81047c4ccb2bd4b72059a4f57a9936446
on3l <110001481+on3l@users.noreply.github.com>
2022-10-04 00:58:44 -0500

    Update ec2-creation.tf
    Added security group name

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "AWSEC2Instance"{
      count   = 2
      ami     =  "ami-06640050dc3f556bb"
      instance_type = "t2.micro"
-     security_groups = ["sg-01b31886923e7aa21"]
+     security_groups = ["DevOps-Ports"]
      key_name        = "ops-key"
      tags = {
         Name = "Tomcat-Srv"
