cc4876fe276d0770026718d0ede64909cca29dd0
on3l <110001481+on3l@users.noreply.github.com>
2022-10-04 00:49:45 -0500

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,16 +1,16 @@
 provider "aws"{
-   region  = "ap-south-1" 
+   region  = "us-east-1" 
    
 }
 
 resource "aws_instance" "AWSEC2Instance"{
      count   = 2
-     ami     =  "ami-05c8ca4485f8b138a"
+     ami     =  "ami-06640050dc3f556bb"
      instance_type = "t2.micro"
-     security_groups = ["launch-wizard-2"]
-     key_name        = "devopsjulybatch2022"
+     security_groups = ["sg-01b31886923e7aa21"]
+     key_name        = "ops-key"
      tags = {
-        Name = "tomcatservers"
+        Name = "Tomcat-Srv"
      }
 }
 
